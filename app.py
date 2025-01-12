193422410b95af83c64995515073f1895ae37db7
slycoopa1 <matheuspinheiro9518@gmail.com>
2025-01-12 21:35:31 +0000

    Car advertisement project comparing car sales.

---
+++ app.py
@@ -0,0 +1,29 @@
+import pandas as pd 
+import numpy as np
+import plotly.express as px
+import streamlit as st
+
+df = pd.read_csv('vehicles_us.csv')
+
+st.header('Old cars are cooler than new cars')
+st.write('They used to have personality')
+
+st.title('Car Sales Dashboard')
+
+if st.checkbox('Show raw data'):
+    st.write(df)
+
+column = st.selectbox(
+    'Select a column to view its distribution',
+    options=['price', 'odometer', 'model_year']
+)
+
+if column:
+    fig = px.histogram(df, x=column)
+    st.plotly_chart(fig)
+
+fig = px.histogram(df, x='price', nbins=20)
+st.plotly_chart(fig)
+
+scatter = px.scatter(df, x='model_year', y='price')
+st.plotly_chart(scatter)
